52109fcbd68e1d4574cc0fb5b5cda64fa482c496
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2021-05-21 17:30:01 +0530

    feat: add tips to the extension

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # pragmaticprogrammertips README
 
-This is the README for your extension "pragmaticprogrammertips". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "pragmatic_programmer_tips". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { window, ExtensionContext } from 'vscode';
+
+import * as quotes from './data/tips.json' ;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(_: ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pragmaticprogrammertips" is now active!');
+	const randomIndex = Math.floor(Math.random() * 70)
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pragmaticprogrammertips.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	window.showInformationMessage(`${quotes[randomIndex].title}! ${quotes[randomIndex].body}`);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from PragmaticProgrammerTips!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
